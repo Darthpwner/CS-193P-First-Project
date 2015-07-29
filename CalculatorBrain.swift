bb2cee3d7125c1ba414e1c083146776159b99eab
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-07-28 23:36:14 -0700

    Finished Lecture 3, but need to review a lot

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -1,82 +1,107 @@
-////
-////  CalculatorBrain.swift
-////  CS 193P First Project
-////
-////  Created by Matthew Allen Lin on 7/18/15.
-////  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
-////
 //
-//import Foundation
+//  CalculatorBrain.swift
+//  CS 193P First Project
 //
-//class CalculatorBrain
-//{
-//    private enum Op
-//    {
-//        case Operand(Double)
-//        case UnaryOperation(String, Double -> Double)
-//        case BinaryOperation(String, (Double, Double) -> Double)
-//    }
-//    
-//    private var opStack = [Op]()
-//    
-//    private var knownOps = [String:Op]()
-//    
-//    init()
-//    {
-//        knownOps["×"] = Op.BinaryOperation("×", *)
-//        knownOps["÷"] = Op.BinaryOperation("÷") {$1 / $0}   //Can't call the operator function because order matters
-//        knownOps["+"] = Op.BinaryOperation("+", +)
-//        knownOps["−"] = Op.BinaryOperation("−") {$1 - $0}   //Can't call the operator function because order matters
-//        knownOps["√"] = Op.UnaryOperation("√", sqrt)
-//    }
+//  Created by Matthew Allen Lin on 7/18/15.
+//  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
 //
-//    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
-//    {
-//        if !ops.isEmpty
-//        {
-//            var remainingOps = ops //Makes a copy that is mutable
-//            let op = remainingOps.removeLast()
-//            switch op
-//            {
-//                case .Operand(let operand):
-//                    return (operand, remainingOps)
-//                case .UnaryOperation(_,let operation):
-//                    let operandEvaluation = evaluate(remainingOps)  //Value is a tuple
-//                    if let operand = operandEvaluation.result  //Type is optional
-//                    {
-//                        return (operation(operand), operandEvaluation.remainingOps)
-//                    }
-//                case .BinaryOperation(_, let operation):
-//                    let op1Evaluation = evaluate(remainingOps)
-//                    if let operand1 = op1Evaluation.result
-//                    {
-//                        let op2Evaluation = evaluate(op1Evaluation.remainingOps)
-//                        if let operand2 = op2Evaluation.result
-//                        {
-//                            return (operation(operand1, operand2), op2Evaluation.remainingOps)
-//                        }
-//                    }
-//        }
-//        
-//        return (nil, ops)   //Default is to return nill
-//    }
-//    
-//    func evaluate() -> Double? //Optional because sometimes there isn't anything on the stack when you evaluate
-//    {
-//        let (result, remainder) = evaluate(opStack)
-//        return result
-//    }
-//    
-//    func pushOperand(operand: Double)
-//    {
-//        opStack.append(Op.Operand(operand))
-//    }
-//    
-//    func performOperation(symbol: String)
-//    {
-//        if let operation = knownOps[symbol]
-//        {
-//            opStack.append(operation)
-//        }
-//    }
-//}
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op: Printable
+    {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String
+        {
+            get
+            {
+                switch self
+                {
+                    case .OPerand(let operand):
+                        return "\(operand)"
+                    case .UnaryOperation(let symbol, _):
+                        return symbol
+                    case .BinaryOperation(let symbol, _):
+                        return symbol
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()
+    
+    init()
+    {
+        func learnOp(op: Op)
+        {
+            knowOps[op.description] = op
+        }
+        
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") {$1 / $0}   //Can't call the operator function because order matters
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") {$1 - $0}   //Can't call the operator function because order matters
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty
+        {
+            var remainingOps = ops //Makes a copy that is mutable
+            let op = remainingOps.removeLast()
+            switch op
+            {
+                case .Operand(let operand):
+                    return (operand, remainingOps)
+                case .UnaryOperation(_,let operation):
+                    let operandEvaluation = self.evaluate(remainingOps)  //Value is a tuple
+                    if let operand = operandEvaluation.result  //Type is optional
+                    {
+                        return (operation(operand), operandEvaluation.remainingOps)
+                    }
+                case .BinaryOperation(_, let operation):
+                    let op1Evaluation = self.evaluate(remainingOps)
+                    if let operand1 = op1Evaluation.result
+                    {
+                        let op2Evaluation = self.evaluate(op1Evaluation.remainingOps)
+                        if let operand2 = op2Evaluation.result
+                        {
+                            return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                        }
+                    }
+        }
+        
+        return (nil, ops)   //Default is to return nill
+    }
+    
+    func evaluate() -> Double? //Optional because sometimes there isn't anything on the stack when you evaluate
+    {
+        let (result, remainder) = self.evaluate(opStack)    //"self" is adopted from Objective-C
+        println("\opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func pushOperand(operand: Double) -> Double?
+    {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+        
+    }
+    
+    func performOperation(symbol: String)
+    {
+        if let operation = knownOps[symbol]
+        {
+            opStack.append(operation)
+        }
+    }
+ 
+};
--- ViewController.swift
+++ ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController
     
     var userIsInTheMiddleOfTypingNumber = false
     
+    var brain = CalculatorBrain()   //Green arrow in Hegarty's model
+    
     @IBAction func appendDigit(sender: UIButton)
     {
         let digit = sender.currentTitle!
@@ -40,64 +42,76 @@ class ViewController: UIViewController
         {
             enter()
         }
-        
-        //Similar to C++
-        switch operation
+
+        if let operation = sender.currentTitle
         {
-            case "ASIN": performSingleOperation{ asin($0) }
-            case "ACOS": performSingleOperation{ acos($0) }
-            case "ATAN": performSingleOperation{ atan($0) }
-            case "FLOOR": performSingleOperation{ floor($0) }
-            case "CEIL": performSingleOperation{ ceil($0) }
-            case "SIN": performSingleOperation{ sin($0) }
-            case "COS": performSingleOperation{ cos($0) }
-            case "TAN":
-                performSingleOperation{ tan($0) }
-            case "LOG": performSingleOperation { log($0) }
-            case "e^x": performSingleOperation{ exp($0) }
-            //Immediates refer to the arguments
-            case "AC": performSingleOperation({ Int in
-                self.operandStack.removeAll(keepCapacity: true)
-                return 0
-            })
-           // case "π": return getPi();
-            case "±": performSingleOperation ({ (Op1: Double) -> Double in
-                return 0 - Op1
-                
-            })
-            case "%": performOperation { $1 % $0 }  //2nd argument modulo by 1st argument
-            case "÷": performOperation { $1 / $0 }  //2nd argument divided by 1st argument
-            case "×": performOperation { $1 * $0 }  //2nd argument times 1st argument
-            case "−": performOperation { $1 - $0 }  //2nd argument minus 1st argument
-            case "+": performOperation { $1 + $0 }  //2nd argument plus 1st argument
-//            case "(":
-//            case ")":
-            case "^": performOperation { pow($1, $0) }
-            case"√": performSingleOperation { sqrt($0) }
-            case ".": performSingleOperation({ (Op1: Double) -> Double in
-                return Op1 * 1.0
-            })  //BUGGY!
-            default: break
+            if let result = brain.performOperation(operation)
+            {
+                displayValue = result
+            }
+            
+            else
+            {
+                displayValue = 0
+            }
         }
+        //Similar to C++
+//        switch operation
+//        {
+//            case "ASIN": performSingleOperation{ asin($0) }
+//            case "ACOS": performSingleOperation{ acos($0) }
+//            case "ATAN": performSingleOperation{ atan($0) }
+//            case "FLOOR": performSingleOperation{ floor($0) }
+//            case "CEIL": performSingleOperation{ ceil($0) }
+//            case "SIN": performSingleOperation{ sin($0) }
+//            case "COS": performSingleOperation{ cos($0) }
+//            case "TAN":
+//                performSingleOperation{ tan($0) }
+//            case "LOG": performSingleOperation { log($0) }
+//            case "e^x": performSingleOperation{ exp($0) }
+//            //Immediates refer to the arguments
+//            case "AC": performSingleOperation({ Int in
+//                self.operandStack.removeAll(keepCapacity: true)
+//                return 0
+//            })
+//           // case "π": return getPi();
+//            case "±": performSingleOperation ({ (Op1: Double) -> Double in
+//                return 0 - Op1
+//                
+//            })
+//            case "%": performOperation { $1 % $0 }  //2nd argument modulo by 1st argument
+//            case "÷": performOperation { $1 / $0 }  //2nd argument divided by 1st argument
+//            case "×": performOperation { $1 * $0 }  //2nd argument times 1st argument
+//            case "−": performOperation { $1 - $0 }  //2nd argument minus 1st argument
+//            case "+": performOperation { $1 + $0 }  //2nd argument plus 1st argument
+////            case "(":
+////            case ")":
+//            case "^": performOperation { pow($1, $0) }
+//            case"√": performSingleOperation { sqrt($0) }
+//            case ".": performSingleOperation({ (Op1: Double) -> Double in
+//                return Op1 * 1.0
+//            })  //BUGGY!
+//            default: break
+//        }
     }
 
-    func performOperation(operation: (Double, Double) -> Double)
-    {
-        if operandStack.count >= 2
-        {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    
-    func performSingleOperation(operation: Double -> Double)
-    {
-        if operandStack.count >= 1
-        {
-            displayValue = operation(operandStack.removeLast())
-            enter()
-        }
-    }
+//    func performOperation(operation: (Double, Double) -> Double)
+//    {
+//        if operandStack.count >= 2
+//        {
+//            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+//            enter()
+//        }
+//    }
+//    
+//    func performSingleOperation(operation: Double -> Double)
+//    {
+//        if operandStack.count >= 1
+//        {
+//            displayValue = operation(operandStack.removeLast())
+//            enter()
+//        }
+//    }
     
 //    func getPi() -> Double
 //    {
@@ -112,15 +126,14 @@ class ViewController: UIViewController
     @IBAction func enter()
     {
         userIsInTheMiddleOfTypingNumber = false
-        
-        if operandStack.first == 0.0    //Clear the top if necessary
+        if let result = brain.pushOperand(displayValue)
         {
-            operandStack.removeLast()
+            brain.pushOperand(displayValue)
         }
-        
-        //Typecast as a double by creating another variable because display.text! is of type "String"
-        operandStack.append(displayValue)
-        println("operandStack = \(operandStack)")
+        else
+        {
+            displayValue = 0
+        }
     }
     
     var displayValue: Double
